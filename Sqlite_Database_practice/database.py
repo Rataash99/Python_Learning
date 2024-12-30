5715cebf24faa483de988aeceb86841455915c64
rahul singh <rohit.singh11299@gmail.com>
2024-12-30 12:47:48 +0530

    updating records using sqlite

--- Sqlite_Database_practice/database.py
+++ Sqlite_Database_practice/database.py
@@ -4,16 +4,43 @@ import sqlite3
 conn = sqlite3.connect('./Sqlite_Database_practice/customer.db')
 
 # create a cursor
-cursor = conn.Cursor()
+cursor = conn.cursor()
 
-cursor.execute("INSERT INTO customers VALUES ('Rohit', 'Singh', 'rohit.singh11299@gmail.com')")
-
-# create a table
+# # create a table
 # cursor.execute("""CREATE TABLE customers (
 #         first_name text,
 #         last_name text,
-#         email text,
+#         email text
 #     )""")
+
+# list of customers
+many_customers = [
+    ("Rahul", "Singh", "rahul.singh@gmail.com"),
+    ("Nitin", "kumar", "nitin.kumar@gmail.com"),
+    ("Simran", "Singh", "simran.singh@gmail.com")
+]
+
+# inserting a single data row
+# cursor.execute("INSERT INTO customers VALUES ('Rohit', 'Singh', 'rohit.singh11299@gmail.com')")
+
+# inserting large data row
+# cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+
+#Query the Databae
+cursor.execute("SELECT rowid, * FROM customers WHERE last_name LIKE 'K%'")
+
+# fetching the first item of the table
+# cursor.fetchone()
+
+# fetching multiple items from the table
+# cursor.fetchmany()
+
+# fetching all items of table
+items = cursor.fetchall()
+
+print(f"S.No \t First_Name \t Last_Name \t Email")
+for item in items:
+    print(f"{item[0]} \t\t {item[1]} \t\t {item[2]} \t\t {item[3]}")
 
 # DATATYPE in sqlite
 # NULL
---
+++ Sqlite_Database_practice/update_records.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("./Sqlite_Database_practice/customer.db")
+
+cursor = conn.cursor()
+
+# verifying the records
+cursor.execute("SELECT * FROM customers WHERE last_name = 'kumar'")
+print("Before updating :-\n", cursor.fetchall())
+
+cursor.execute("""UPDATE customers SET first_name = 'Naman' WHERE last_name = 'kumar'
+               """)
+conn.commit()
+
+cursor.execute("SELECT rowid, * FROM customers")
+
+items = cursor.fetchall()
+
+print(f"S.No \t First_Name \t Last_Name \t Email")
+for item in items:
+    print(f"{item[0]} \t\t {item[1]} \t\t {item[2]} \t\t {item[3]}")
