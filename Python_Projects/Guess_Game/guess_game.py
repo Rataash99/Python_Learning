b2076229b15895460ef6a224bbd844179fd0fae9
rahul singh <rohit.singh11299@gmail.com>
2025-01-01 18:07:03 +0530

    small projects guess_game and quiz game

---
+++ Python_Projects/Guess_Game/guess_game.py
@@ -0,0 +1,24 @@
+import random
+
+
+def generate_random_guess(guess):
+    random_guess = random.randint(0, guess)
+
+    if(random_guess == guess):
+        return True
+    else:
+        return False
+    
+while True:
+    user_guess = input("\nHey guess a number : ")
+    if(not user_guess.isdigit()):
+        print("\nType a number next time!\n")
+        continue
+
+    check = generate_random_guess(int(user_guess))
+    if(check):
+        print("\nyoohoo! you got it correct! <(^_^)\n")
+        break
+
+    else:
+        print("you got it wrong! :(\n")
